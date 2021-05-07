676e5a827766801471e71a1f846dcce0d6629044
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-05-07 12:24:37 -0400

    table view for admin users

--- VirtualGallery/Controllers/AdminUserController.cs
+++ VirtualGallery/Controllers/AdminUserController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VirtualGallery.Models;
 
 namespace VirtualGallery.Controllers
 {
     public class AdminUserController : Controller
     {
+        private AdminUserDbContext _db = new AdminUserDbContext();
         // GET: AdminUser
         public ActionResult Index()
         {
-            return View();
+            return View(_db.AdminUsers.ToList());
         }
     }
 }
