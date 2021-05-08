53f2ee529588c20015d68f7a94e8f101325547e5
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-05-07 23:20:42 -0400

    create exhibition

--- VirtualGallery/Controllers/ExhibitionController.cs
+++ VirtualGallery/Controllers/ExhibitionController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VirtualGallery.Models;
 
 namespace VirtualGallery.Controllers
 {
     public class ExhibitionController : Controller
     {
+        private ExhibitionDbContext _db = new ExhibitionDbContext();
         // GET: Exhibition
         public ActionResult ExhibitionIndex()
         {
+            return View(_db.Exhibitions.ToList());
+        }
+
+        //GET : Exhibition/Create
+        public ActionResult CreateExhibition()
+        {
             return View();
+        }
+       //POST: Exhibition/Create
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+       public ActionResult CreateExhibition(Exhibition exhibition)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Exhibitions.Add(exhibition);
+                _db.SaveChanges();
+                return RedirectToAction("ExhibitionIndex");
+            }
+            return View(exhibition);
         }
     }
 }
